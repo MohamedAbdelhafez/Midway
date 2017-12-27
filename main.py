feb1bfd5c0bc37f710538370ece8d0c83f49cc46
Mohamed <abdelhafez@uchicago.edu>
2017-12-27 02:38:43 -0600

    Slurm right

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--nodes", help="assign nodes id for this run",default='0')
+args = parser.parse_args()
+nodes = args.nodes
 f = open('out.txt','w')
-f.write(str($SLURM_JOB_NODELIST))
+f.write(str(nodes))
 f.close()
