602529c41e05542870f7c5d78de237bc70a92c4e
Mohamed <abdelhafez@uchicago.edu>
2017-12-27 03:07:21 -0600

    mm

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ args = parser.parse_args()
 nodes = str(args.nodes)
 start_node = int(nodes[7:10])
 end_node = int(nodes[11:14])
+f = open('out.txt','w')
 for ii in range (end_node - start_node):
-    f = open('out.txt','w')
+    
     f.write(str(start_node+ii) + " ")
-    f.close()
+    
+f.close()
