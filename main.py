e721b7b317f9c7d36678f83305a94d7ab4724290
Mohamed <abdelhafez@uchicago.edu>
2017-12-27 03:05:22 -0600

    strings

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--nodes", help="assign nodes id for this run",default='0')
 args = parser.parse_args()
-nodes = args.nodes
-f = open('out.txt','w')
-f.write(str(nodes))
-f.close()
+nodes = str(args.nodes)
+start_node = int(nodes[7:10])
+end_node = int(nodes[11:14])
+for ii in range (end_node - start_node):
+    f = open('out.txt','w')
+    f.write(str(start_node+ii) + " ")
+    f.close()
