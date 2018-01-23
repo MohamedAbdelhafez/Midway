8dcfbc18a9077f52aefad36b9706ad1ed04f2577
Mohamed <abdelhafez@uchicago.edu>
2018-01-23 15:24:47 -0600

    a

--- main3.py
+++ main3.py
@@ -12,7 +12,7 @@
     
 #f.close()
 import numpy as np
-import os,sys,inspect
+import os,sys,inspect, subprocess
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,currentdir+"/Packages") 
@@ -22,6 +22,7 @@ sys.path.insert(0,currentdir+"/Packages")
 print ("node_id : " + str(os.environ['SLURM_NODEID']))
 print ("task_id : " + str(os.environ['SLURM_PROCID']))
 print ("host_name : " + str(os.environ['HOSTNAME']))
+print("-->" + repr(subprocess.check_output("hostname")))
 
 
 #task_index  = int( os.environ['SLURM_PROCID'] )
