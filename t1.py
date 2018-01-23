71c1ed9d9011f4d02a78b137423a1e54dd9110ff
Mohamed <abdelhafez@uchicago.edu>
2018-01-23 15:16:50 -0600

    s

--- t1.py
+++ t1.py
@@ -1,7 +1,16 @@
 #!/bin/env python 
 
-import os 
-import subprocess 
 
-print(os.environ["HOSTNAME"]) 
-#print("-->" + repr(subprocess.getstatusoutput("hostname")))
+import numpy as np
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,currentdir+"/Packages") 
+
+
+
+print ("node_id : " + str(os.environ['SLURM_NODEID']))
+print ("task_id : " + str(os.environ['SLURM_PROCID']))
+print ("host_name : " + str(os.environ['HOSTNAME']))
+print("-->" + repr(subprocess.check_output("hostname", universal_newlines=True, stderr=STDOUT)))
+ 
