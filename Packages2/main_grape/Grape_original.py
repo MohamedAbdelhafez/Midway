8071df5c0495db636d025e6186b291629e189f11
Mohamed <abdelhafez@uchicago.edu>
2018-01-24 13:58:41 -0600

    d

--- Packages2/main_grape/Grape_original.py
+++ Packages2/main_grape/Grape_original.py
@@ -104,7 +104,7 @@ def Grape(H0,Hops,Hnames,U,total_time,steps,states_concerned_list,convergence = 
         print ("entering iterations")
         for ii in range(convergence['max_iterations']):
             learning_rate = float(convergence['rate']) * np.exp(-float(ii) / convergence['learning_rate_decay'])
-            print('\r'+' Iteration: ' +str(ii) + ": Running batch #" +str(tfs.task_id+1)+" out of "+str(num_batches)+ " with "+str(num_traj_batch)+" jump trajectories")
+            print('\r'+' Iteration: ' +str(ii) + ": Running batch #" +str(tfs.task_index+1)+" out of "+str(num_batches)+ " with "+str(num_traj_batch)+" jump trajectories")
             sys.stdout.flush()
             feed_dict = {tfs.learning_rate: 0, self.tfs.start: np.zeros([num_psi0]), self.tfs.end: np.ones([num_psi0]), self.tfs.num_trajs:num_traj_batch*np.ones([num_psi0])}
             norms, expects, l1d,l2d,  quad, l1, l2, inter_vecs = sess.run([tfs.norms, tfs.expectations, tfs.Il1d, tfs.Il2d,tfs.quad, tfs.Il1, tfs.Il2, tfs.inter_vecs], feed_dict=self.feed_dict)
