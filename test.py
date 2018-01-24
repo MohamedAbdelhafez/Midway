251cc496a3da98642815695f194553da4ba0b008
Mohamed <abdelhafez@uchicago.edu>
2018-01-24 12:22:52 -0600

    s

---
+++ test.py
@@ -0,0 +1,114 @@
+#!/bin/env python 
+
+import numpy as np
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,currentdir+"/Packages2") 
+from main_grape.Grape_original import Grape
+
+#Defining time scales
+total_time = 5
+steps = 50
+state_transfer = True
+RWA = True
+RFT = True
+
+#Defining H0
+
+
+
+
+
+qubit_state_num = 2
+
+fq= 4.6/(2*np.pi)
+kappa = 0.05
+gamma = 0.001
+g = 0.05
+
+mode_state_num = 5
+#g = 2.*np.pi*0.1 #GHz
+fc = 5.0/(2*np.pi) #GHz
+state_num = qubit_state_num * mode_state_num
+if RFT:
+    fq = fq-fc
+    fc = 0
+    
+wc = 2*np.pi*fc
+wa = 2*np.pi*fq
+
+
+alpha = 0.224574
+ens = np.array([ 2*np.pi*ii*(fq - 0.5*(ii-1)*alpha) for ii in np.arange(qubit_state_num)])
+H0q = np.kron(np.identity(mode_state_num),np.diag(ens))
+
+a   = np.kron(np.diag(np.sqrt(np.arange(1,mode_state_num)),1),np.identity(qubit_state_num))
+adag   = np.kron(np.diag(np.sqrt(np.arange(1,mode_state_num)),-1),np.identity(qubit_state_num))
+sm = np.kron(np.identity(mode_state_num),np.diag(np.sqrt(np.arange(1,qubit_state_num)),1))
+smdag = np.kron(np.identity(mode_state_num),np.diag(np.sqrt(np.arange(1,qubit_state_num)),-1))
+
+if RWA:
+     H0 = wc * np.dot(adag,a) + H0q + g * (np.dot(adag,sm) + np.dot(a,smdag))
+else:
+     H0 = wc * np.dot(adag,a) + H0q +  g * np.dot((adag + a),(sm + smdag))
+#Defining Forbidden sates
+
+
+#Defining Concerned states (starting states)
+psi0=[0,1]
+
+#Defining states to include in the drawing of occupation
+states_draw_list = [0,1,2]
+states_draw_names = ['g0','e0','g1']
+
+
+
+U =[]
+U1 = np.zeros(state_num,dtype=complex)
+U1[1]=1
+U1[0]=0
+U.append(U1)
+U2 = np.zeros(state_num,dtype=complex)
+U2[0]=1
+U.append(U2)
+    
+
+#Defining U0 (Initial)
+q_identity = np.identity(qubit_state_num)
+U0= q_identity
+
+#Defining control Hs
+IX = a + adag
+IY = (0+1j)* (a-adag)
+Hops = [IX]
+ops_max_amp = [0.05]
+Hnames =['HI']
+
+#Defining convergence parameters
+max_iterations = 5000
+decay = max_iterations/2
+convergence = {'rate':0.005, 'update_step':10, 'max_iterations':max_iterations,\
+               'conv_target':1e-6,'learning_rate_decay':decay}
+reg_coeffs = {'envelope' : 0,  'dwdt':0,'d2wdt2':0}
+
+
+c_ops=[]
+c_ops.append(np.sqrt(gamma)*sm)
+c_ops.append(np.sqrt(kappa)*a)
+
+u0 = None
+
+print ("Parameters Defined")
+
+
+
+      
+uks,U_final = Grape(H0,Hops,Hnames,U,total_time,steps,psi0,convergence=convergence, draw = [states_draw_list,states_draw_names],  
+                    
+                    show_plots = False, c_ops = c_ops, initial_guess = u0, use_gpu = True,
+       unitary_error = 1e-4,  maxA=ops_max_amp, state_transfer = state_transfer, method ='Adam', expect_op = IY,
+                    reg_coeffs=reg_coeffs, file_name='JC', trajectories = 10000, do_all_traj = False,
+                    data_path = str(currentdir)+'/Data')
+
+######################################################################################################################
