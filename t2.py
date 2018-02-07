2187c7c13d80702e03ab14a4a010f63adbd53019
Mohamed <abdelhafez@uchicago.edu>
2018-02-06 23:45:21 -0600

    x

--- t2.py
+++ t2.py
@@ -54,7 +54,7 @@ def run_training(server, cluster_spec, num_workers, task_index) :
                 global_step = global_step)
             # Create a session for running Ops on the Graph.
             config = tf.ConfigProto(allow_soft_placement = True, device_filters=[
-    '/job:ps', '/job:worker/task:%d' % task_index)
+    '/job:ps', '/job:worker/task:%d' % task_index])
             sess = sv.prepare_or_wait_for_session(server.target, config = config)
 
             if is_chief:
